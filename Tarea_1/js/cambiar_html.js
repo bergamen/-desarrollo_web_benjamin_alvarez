4c91a0fb5a88317e6c2b54e4e75a6a781ceb01ad
Benjamín Álvarez H. <alvarezbenjamin8@gmail.com>
2025-09-05 23:13:09 -0400

    Feat: archivos tarea

---
+++ Tarea_1/js/cambiar_html.js
@@ -0,0 +1,8 @@
+
+const go_to_add_adoption = () => {
+    window.location.href = "../html/principal.html";
+}
+
+
+let volver = document.getElementById("volver");
+volver.addEventListener("click",go_to_add_adoption);
